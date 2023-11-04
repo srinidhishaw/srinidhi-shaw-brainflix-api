a56f736b366fb0b158e7d9670713ee2a42f59d5f
Srinidhi Shaw <srinidhishaw@gmail.com>
2023-11-04 18:23:16 -0400

    set up the post functionality

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require("express")
 // const fs = require("fs");
 const cors = require("cors")
+
 require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+
 const app=express()
+app.use(express.json());
+app.use(express.static("public"));
 app.use(cors({origin:process.env.FRONTEND_URL}))
+
 function logRequestInfo(req, res, next) {
     console.log(`Request received at ${new Date()} for path: ${req.path}`);
     next();
   }
   app.use(logRequestInfo);
-app.listen(8080, function(){
-    console.log("listening on port 8080")
-})
+
 const vidRoute = require("./routes/videos")
 app.use("/videos",vidRoute)
+
+app.listen(PORT, function(){
+  console.log("listening on port 8080")
+})
 // app.get("/videos",function(req,res){
 //     res.send("hi")
 // })
--- routes/videos.js
+++ routes/videos.js
@@ -2,42 +2,41 @@ const express = require("express")
 const router = express.Router();
 // const video = require("../data/videos.json")
 const fs = require("fs");
+const{v4:uuid} = require("uuid")
 
-const vid = fs.readFileSync("./data/videos.json")
+
+
+router.get("/",function(_req,res){
+    const vid = fs.readFileSync("./data/videos.json")
 const vidParsed = JSON.parse(vid);
-// let vidList = []
-//     video.map(item=>{
-//         let obj={
-//             id: item.id,
-//             title: item.title,
-//             channel: item.channel,
-//             image: item.image
-//         };
-//         vidList.push(obj)
-//     })
-router.get("/",function(req,res){
     
     res.send(vidParsed)
 
 })
 
 router.post("/",function(req,res){
+    const vid = fs.readFileSync("./data/videos.json")
+const vidParsed = JSON.parse(vid);
     const {title, description} = req.body;
-    const img = app.use(express.static("../public/images/Upload-video-preview.jpg"))
+    const img = express.static("./images/Upload-video-preview.jpg")
     const newVid = {
         id:uuid(),
         title:title,
         description:description,
         image:img
     }
+    console.log(newVid)
     vidParsed.push(newVid)
     const vidUpdated = JSON.stringify(vidParsed);
     fs.writeFileSync("./data/videos.json",vidUpdated)
-
-    res.status(201).json(newVid)
+    res.send(vidParsed)
+    res.status(201)
+    
 })
 
 router.get("/:id",function(req,res){
+    const vid = fs.readFileSync("./data/videos.json")
+const vidParsed = JSON.parse(vid);
     const id = req.params.id
     const obj = vidParsed.find(vid => vid.id===id)
     res.send(obj)
