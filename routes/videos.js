c66bdbee4a3b2623aa5b85198d8d64bc2f82023d
Srinidhi Shaw <srinidhishaw@gmail.com>
2023-11-05 01:58:49 -0500

    update videos.json file. add placeholders for post video

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const router = express.Router();
-// const video = require("../data/videos.json")
 const fs = require("fs");
 const{v4:uuid} = require("uuid")
 
@@ -18,14 +17,18 @@ router.post("/",function(req,res){
     const vid = fs.readFileSync("./data/videos.json")
 const vidParsed = JSON.parse(vid);
     const {title, description} = req.body;
-    // const img = express.static("./images/Upload-video-preview.jpg")
     const newVid = {
         id:uuid(),
         title:title,
+        channel:"MTV",
+        image:"./images/Upload-video-preview.jpg",
         description:description,
-        image:"./images/Upload-video-preview.jpg"
+        views:"1,001,023",
+        likes:"110,985",
+        duration:"4:01",
+        comments:[{id:"35bba08b-1b51-4153-ba7e-6da76b5ec1b9",name:"Micheal Lyons",comment:"They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.","likes":0,"timestamp":1628522461000}]
+        
     }
-    console.log(newVid)
     vidParsed.push(newVid)
     const vidUpdated = JSON.stringify(vidParsed);
     fs.writeFileSync("./data/videos.json",vidUpdated)
